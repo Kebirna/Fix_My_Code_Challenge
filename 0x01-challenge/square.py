ee8f77840e411f8b7cc297f483b4638413dff3dc
dekenzo <79919801+Kebirna@users.noreply.github.com>
2022-10-11 18:32:39 +0800

    Update square.py

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,38 +1,33 @@
 #!/usr/bin/python3
-"""
-A Module for modeling Square obj
-"""
+""" Module for square class"""
 
 
-class square():
-    """ A class Square
-    """
+class Square():
+    """ Square class """
+    width = 0
+    height = 0
 
-    def __init__(self, width, height):
-        """
-        initialize
-        """
-        self.width = width
-        self.height = height
+    def __init__(self, *args, **kwargs):
+        """ Instantiation of class """
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
         return self.width * self.height
 
-    def PermiterOfMySquare(self):
-        """ Perimeter of the square """
+    def permiter_of_my_square(self):
+        """ Perimeter of my square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """ String format of square """
+        """ Printable representation """
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
-    """
-    deriver code
-    """
-    s = square(width=12, height=9)
+    """ Create a square object """
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.permiter_of_my_square())
